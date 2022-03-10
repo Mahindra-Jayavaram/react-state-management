4ad3bb10426a322c378e09ddf1a6eb60b625e38a
Mahindra Jayavaram <mahindrajayavaram@gmail.com>
2022-03-10 16:30:16 +0530

    final commit

--- src/components/GroceryInput.jsx
+++ src/components/GroceryInput.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
+// export const GroceryInput = () =>{
 export const GroceryInput = ({addGrocery}) =>{
     const [text, setText] = useState("")
-return <div>
+return <> <div>
     <input type= "text" onChange = {(e)=>{
+        // console.log("value:", e.target.value)
         setText(e.target.value)
     }} />
     <button onClick ={ () =>{
@@ -11,6 +13,11 @@ return <div>
     }}>
         Add Grocery
         </button>
+
+
 </div>
+{text}
+
+</>
 
 }
